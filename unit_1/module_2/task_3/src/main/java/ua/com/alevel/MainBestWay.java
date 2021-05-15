21d04adcecaf4f4c76ce29070ed2add9cd2cd437
Artem <artem.kuzmin@nure.ua>
2021-05-15 21:17:04 +0300

    module 2

--- unit_1/module_2/task_3/src/main/java/ua/com/alevel/MainBestWay.java
+++ unit_1/module_2/task_3/src/main/java/ua/com/alevel/MainBestWay.java
@@ -3,8 +3,13 @@ package ua.com.alevel;
 import ua.com.alevel.bestway.Graph;
 import ua.com.alevel.bestway.Node;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class MainBestWay {
 
@@ -13,10 +18,22 @@ public class MainBestWay {
 
         int V = 5;
         int source = 0;
-        String[] names = {"Seattle", "New York", "San Francisco", "Chicago", "Dallas", "Miami"};
+        String[] names = null;
 
         // Adjacency list representation of the connected edges
         List<List<Node>> adj = new ArrayList<>();
+
+        File fileInput = new File("input.txt");
+        File fileOutput = new File("output.txt");
+
+        // Read data from file
+        try (
+                Scanner input = new Scanner(fileInput)
+        ) {
+            names = input.nextLine().split(", ");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         // Initialize list for every node
         for (int i = 0; i < V; i++) {
@@ -36,6 +53,18 @@ public class MainBestWay {
         // Calculate the single source shortest path
         Graph graph = new Graph(V);
         graph.dijkstra(adj, source);
+
+        // Write data to file
+        try (
+                FileWriter output = new FileWriter(fileOutput)
+        ) {
+            output.write("The shortest path from:\n");
+            for (int i = 0; i < graph.dist.length; i++) {
+                output.write(names[0] + " to " + names[i] + " is " + graph.dist[i] + "\n");
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
 
         // Print the shortest path to all the nodes from the source node
         System.out.println("The shortest path from:");
